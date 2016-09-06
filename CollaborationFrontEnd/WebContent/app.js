1573dc039acfaa7926bd25da766f6b8b186c1ff4
Vaibhav <vibes87@gmail.com>
2016-09-06 16:03:06 +0530

    Collaboration

--- CollaborationFrontEnd/WebContent/app.js
+++ CollaborationFrontEnd/WebContent/app.js
@@ -1,22 +1,3 @@
-var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
-    $routeProvider
-    .when("/", {
-        templateUrl : "index.html",
-    })
-    .when("/blog", {
-        templateUrl : "Blog.html",
-        //controller : "blogCtrl"
-    })
-    .when("/chat", {
-        templateUrl : "Chat.html",
-        //controller : "chatCtrl"
-    });
-});
-// app.controller("blogCtrl", function ($scope) {
-//     $scope.msg = "I love London";
-// });
-// app.controller("chatCtrl", function ($scope) {
-//     $scope.msg = "I love Paris";
-// });
-
+'use strict';
+ 
+var App = angular.module('myApp',[]);
